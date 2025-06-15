18af9b6cc1c84858000a18966f518b800cf914d3
Kandika Prima Putra <84494833+kandikaprima@users.noreply.github.com>
2025-06-15 22:56:08 +0800

    Delete frontend directory

--- frontend/lib/core/constants/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color yellow = Color(0xFFFFD500);
-  static const Color blue = Color(0xFF00509D);
-  static const Color background = Colors.white;
-  static const Color black = Colors.black;
-}
