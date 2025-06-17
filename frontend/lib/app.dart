5a0f26d7751bf4711cb48466c169ccd8393a2c34
Kandika Prima Putra <84494833+kandikaprima@users.noreply.github.com>
2025-06-18 02:03:19 +0800

    fix: Add muhsroom repositories

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'logic/detection_bloc/detection_bloc.dart';
+import 'package:frontend/data/repositories/mushroom_repository.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,7 +12,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => DetectionBloc()),
+        BlocProvider(create: (_) => DetectionBloc(repository: MushroomRepository())),
       ],
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
