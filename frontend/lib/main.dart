8a0fbdd199490f103b413fa94514857f75cd76ca
Kandika Prima Putra <84494833+kandikaprima@users.noreply.github.com>
2025-06-18 02:51:50 +0800

    refactor: Merge app.dart with main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/data/repositories/mushroom_repository.dart';
+
+import 'core/theme/app_theme.dart';
+import 'core/router/app_router.dart';
+import 'logic/detection_bloc/detection_bloc.dart';
 
 void main() {
   runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => DetectionBloc(repository: MushroomRepository())),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light,
+        routerConfig: AppRouter.router,
+      ),
+    );
+  }
 }
